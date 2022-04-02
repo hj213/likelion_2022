13624d5c1e5524cff443b138b398523c6603eb75
hj213 <0213yhj@naver.com>
2022-04-02 15:34:14 +0900

    hello

---
+++ 새 폴더/sfp.c
@@ -0,0 +1,43 @@
+#include "sfp.h"
+#include <stdlib.h>
+sfp int2sfp(int input){
+	//배열로 나머지값 받아서 추가 -> 이진수 
+    //길이 빼기 -1 -> E
+    //
+
+    int data = 32;
+    int ssArr[];
+    int expArr[];
+    int fracArr[];
+
+    while(num>0) 
+    {
+        
+    }
+}
+
+int sfp2int(sfp input){
+
+}
+
+sfp float2sfp(float input){
+}
+
+float sfp2float(sfp input){
+}
+
+sfp sfp_add(sfp a, sfp b){
+	
+}
+
+sfp sfp_mul(sfp a, sfp b){
+	
+}
+
+char* sfp2bits(sfp result){
+
+}
+
+char* sfp_comp(sfp a, sfp b){
+
+}
